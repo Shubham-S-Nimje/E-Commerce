d663235a3f6d42136e6498b46e344305af6b18a4
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-06-16 07:21:37 +0530

    change nav link

--- src/Components/NavbarData/NavbarMain/NavbarCategories.js
+++ src/Components/NavbarData/NavbarMain/NavbarCategories.js
@@ -2,7 +2,7 @@ import React from 'react'
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { Link } from 'react-router-dom';
 
 const NavbarCategories = () => {
   return (
@@ -10,18 +10,18 @@ const NavbarCategories = () => {
     <Navbar.Toggle aria-controls="navbarScroll"/>
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
+            className="me-auto my-2 my-lg-0" 
             navbarScroll
           >
-            <Nav.Link href="/E-Commerce">Home</Nav.Link>
-            <Nav.Link href="/E-Commerce/store">Store</Nav.Link>
-            <Nav.Link href="/E-Commerce/about">About</Nav.Link>
-            <NavDropdown title="Dropdown" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action1">Option 1</NavDropdown.Item>
-              <NavDropdown.Item href="#action2">Option 2</NavDropdown.Item>
-              <NavDropdown.Item href="#action5">Option 2</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="/E-Commerce/contactus">Contact us</Nav.Link>
+            <Link to="/E-Commerce" className='text-white text-decoration-none'>Home</Link>
+            <Link to="/E-Commerce/store" className='text-white text-decoration-none'>Store</Link>
+            <Link to="/E-Commerce/about" className='text-white text-decoration-none'>About</Link>
+            <select id="navbarScrollingDropdown" className='bg-dark border-0 text-white'>
+              <option to="#action1">Option 1</option>
+              <option to="#action2">Option 2</option>
+              <option to="#action5">Option 2</option>
+            </select>
+            <Link to="/E-Commerce/contactus" className='text-white text-decoration-none'>Contact us</Link>
           </Nav>
         </Navbar.Collapse>
         </>
